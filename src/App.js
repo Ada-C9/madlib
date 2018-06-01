38cd0fbb80d5922191303312ca5d3df34be9046b
LetiTran <leticiastran@gmail.com>
2018-06-01 14:12:57 -0700

    Set madlib to be randomly generated, added a method to remove the 'hidden' class of story so it only displays it after submiting the form and passed this method as props for WordsForm

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
     super();
 
     this.state = {
-      selectedMadLib: MadLibs[0]
+      // selectedMadLib: MadLibs[0]
+      selectedMadLib: MadLibs[Math.floor(Math.random() * MadLibs.length)]
     };
   }
 
@@ -26,21 +27,35 @@ class App extends Component {
     this.setState({selectedMadLib: updatedMadLib});
   }
 
+  showStory = () => {
+    // console.log("working");
+    let element = document.getElementById("story");
+    console.log(element);
+    element.classList.remove("hidden");
+  }
+
   render() {
     return (
       <section className="App">
-        <h1>Welcome to MadLibs!</h1>
-        <p>Fill in all of the choices to see your final story.</p>
-        {
-          /*  Render your form with input values */
-        <WordsForm words={this.state.selectedMadLib.words} updateWord={this.updateWord}/>}
-        <Story
-          title={ this.state.selectedMadLib.title }
-          text={ this.state.selectedMadLib.getText() }
-          />
-      </section>
-    );
-  }
-}
+      <h1>Welcome to MadLibs!</h1>
+      <p>Fill in all of the choices to see your final story.</p>
+      {
+        /*  Render your form with input values */
+        <WordsForm
+        words={this.state.selectedMadLib.words} updateWord={this.updateWord}
+        showStory={this.showStory}
+        />}
 
-export default App;
+        <div id="story" className="hidden">
+        <Story
+        title={ this.state.selectedMadLib.title }
+        text={ this.state.selectedMadLib.getText() }
+        />
+        </div>
+
+        </section>
+      );
+    }
+  }
+
+  export default App;
