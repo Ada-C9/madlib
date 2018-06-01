192b3d0a91f7259c9f6005da8424cc037d3a6a2b
Alexandria Brown <axbn2190@gmail.com>
2018-06-01 16:25:10 -0700

    able to type in form input fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,23 +2,41 @@ import React, { Component } from 'react';
 
 class Form extends Component {
 
-  constructor(prop) {
-    super(prop)
+  constructor() {
+    super()
+
+    this.state = {
+      // inputWord: ''
+    }
 
   }
 
-  render() {
+  onFieldChange = (key, value) => {
+    const updatedState = {}
+    updatedState[key] = value;
 
+    this.setState(updatedState);
+  }
+
+  render() {
     return (
       <div>
         <form>
-          <label htmlFor="label">{this.props.label}</label>
-          <input/>
+          <label htmlFor="label">{this.props.wordLabel}: </label>
+          <input
+            name="label"
+            value={this.state.inputWord}
+            onChange={(event) => {this.onFieldChange('label', event.target.value)}}
+          />
         </form>
       </div>
     );
   }
 }
 
-
+// <input
+//   // className="button success"
+//   type="submit"
+//   value="Submit"
+//   />
 export default Form;
