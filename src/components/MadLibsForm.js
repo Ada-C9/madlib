23ce4dc52a9dfbcefca6ca458ab52e159e7962ef
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-06-01 14:52:03 -0700

    Implement OnInputChange function for dynamic form.

--- src/components/MadLibsForm.js
+++ src/components/MadLibsForm.js
@@ -6,13 +6,34 @@ class MadLibsForm extends Component {
     wordsNeeded: PropTypes.array.isRequired,
   };
 
+  constructor (props) {
+    super();
+    this.state = {
+      wordsNeeded: props.wordsNeeded
+    };
+  }
+
+  onInputChange = (event) => {
+    let updatedWords = this.state.wordsNeeded;
+    let updatedWord = updatedWords.find((wordObject) => {
+      return wordObject.key === event.target.name;
+    });
+    updatedWord.value = event.target.value;
+    this.setState({wordsNeeded: updatedWords});
+  }
+
+  onFormSubmit = (event) => {
+    // event.preventDefault();
+    // call method in App.js passed through props that updates the value of the MadLibs
+  }
+
   render() {
     const wordInputs = this.props.wordsNeeded.map((wordObject) => {
       return(<div>
         <input
         key={wordObject['key']}
         name={wordObject['key']}
-        placeholder={wordObject['label']} />
+        placeholder={wordObject['label']} onChange={ this.onInputChange } />
       </div>);
     });
 
