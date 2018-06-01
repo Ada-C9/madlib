bbc3c8f98b1d35ff1f6235ec99019e04496e2a51
Brenda Rios <riosbrenda@gmail.com>
2018-06-01 14:44:15 -0700

    Added the render the form wiht input values in App.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import MadLibs from './madlibs/MadLibs.js';
 import Story from './components/Story.js';
+import StoryForm from './components/StoryForm.js';
 
 class App extends Component {
   constructor() {
@@ -10,8 +11,9 @@ class App extends Component {
     this.state = {
       selectedMadLib: MadLibs[0]
     };
-  }
 
+    this.updateWord = this.updateWord.bind(this)
+  }
   // Update the value of a word in the selected
   // mad lib using setState
   updateWord(key, value) {
@@ -28,9 +30,10 @@ class App extends Component {
       <section className="App">
         <h1>Welcome to MadLibs!</h1>
         <p>Fill in all of the choices to see your final story.</p>
-        {/*
-          Render your form with input values
-        */}
+        <StoryForm
+          updateWord={this.updateWord}
+          words = {this.state.selectedMadLib.words}
+        />
         <Story
           title={ this.state.selectedMadLib.title }
           text={ this.state.selectedMadLib.getText() }
