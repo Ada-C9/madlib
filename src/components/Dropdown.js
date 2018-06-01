955efbce0ef262cfe3f7244993bd1187ea66033a
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-06-01 15:48:57 -0700

    Use select tag to display story options.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import MadLibs from './../madlibs/MadLibs.js';
 
 class Dropdown extends Component {
+
+  selectMadLib = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     const info = MadLibs.map((story, key) => {
-      return <div key={key}>{story.title}</div>
+      return <option key={key} value={story.title}>{story.title}</option>
     });
 
     return (
-      <section>
-        <div>Dropdown</div>
-        <div>
+      <section onChange={ this.selectMadLib }>
+        <select>
+          <option disabled selected value> -- select an option -- </option>
           { info }
-        </div>
+        </select>
       </section>
     );
   }
