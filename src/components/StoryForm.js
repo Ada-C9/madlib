331e5822cd902da82371f46beedf8aa6f740ff00
Brenda Rios <riosbrenda@gmail.com>
2018-06-01 14:16:56 -0700

    Created StoryForm component.

---
+++ src/components/StoryForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './StoryForm.css';
+
+class StoryForm extends Component {
+
+
+
+  render() {
+
+    return (
+
+    );
+  }
+}
+
+export default StoryForm;
