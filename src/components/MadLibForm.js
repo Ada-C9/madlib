808933c5a392d1d55f1f1eb34979a82ad6eddf6a
Karinna Iniguez <karinnainiguez@gmail.com>
2018-06-01 14:28:37 -0700

    Add file for new MadLibForm component and make selected madlib random

---
+++ src/components/MadLibForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MadLibForm extends Component {
+
+
+
+	render(){
+		return(
+			<h2>HELLOOOOOO KARINNA</h2>
+		)
+	}
+}
+
+
+
+
+export default MadLibForm;
