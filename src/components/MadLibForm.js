8c80cf00d537bb4e856008dbe0115f5cd5db93b4
Karinna Iniguez <karinnainiguez@gmail.com>
2018-06-01 15:56:45 -0700

    Generate form and return in render function

--- src/components/MadLibForm.js
+++ src/components/MadLibForm.js
@@ -3,11 +3,41 @@ import PropTypes from 'prop-types';
 
 class MadLibForm extends Component {
 
+	static propTypes = {
+		wordCollection: PropTypes.array.isRequired,
+		updateWordCallback: PropTypes.func.isRequired
+	}
+
+	constructor(props) {
+		super();
+		this.state = {
+			words: props.wordCollection,
+			updateWordCallback: props.updateWordCallback
+		}
+	}
+
+
 
 
 	render(){
+
+		const wordsGenerated = this.props.wordCollection.map((word, index) => {
+			return <div key={index}>
+				<input
+					name={word.key}
+					type="text"
+					value={this.state.value}
+					placeholder={word.label}
+					onChange={this.onInputChange}
+					/>
+			</div>
+		});
+
 		return(
-			<h2>HELLOOOOOO KARINNA</h2>
+			<form onSubmit={this.onFormSubmit}>
+				{wordsGenerated}
+				<input type="submit" value="Make A MadLib!"/>
+			</form>
 		)
 	}
 }
