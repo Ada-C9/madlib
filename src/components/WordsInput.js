a356816ace7d2ac7e8e978557edb5db1ed907071
Abiaina <abinnet.a@gmail.com>
2018-06-01 16:34:37 -0700

    added functions in form to listen for click events and call update madlib word function

--- src/components/WordsInput.js
+++ src/components/WordsInput.js
@@ -3,65 +3,105 @@ import PropTypes from 'prop-types';
 import './WordsInput.css';
 
 class WordsInput extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      adjective_1: '',
+      adjective_2: '',
+      noun_1: '',
+      noun_2: '',
+    };
+  }
+
   static propTypes = {
     words: PropTypes.array.isRequired,
     updateWord: PropTypes.func.isRequired,
 
   }
 
+  onFieldChange = (key, value) => {
+    const updatedState = {}
+    updatedState[key] = value;
+
+    this.setState(updatedState);
+  }
+
+  onSubmit = (event) => {
+  event.preventDefault();
+
+  const madLibInfo = {
+    adjective_1: this.state.adjective_1,
+    adjective_2: this.state.adjective_2,
+    noun_1: this.state.noun_1,
+    noun_2: this.state.noun_2,
+  }
+  
+  this.props.updateWord(madLibInfo);
+
+  this.setState({
+    adjective_1: '',
+    adjective_2: '',
+    noun_1: '',
+    noun_2: '',
+  });
+}
+
+
   render() {
     return (
       <div>
 
       <div>
-        <form  onSubmit={ this.onSubmit } className="new-student-form">
-          <div>
-            <p>
-              <label htmlFor="adjective_1">adjective_1:</label>
-              <input
-                adjective_1 = "adjective_1"
-                onChange={(event) => { this.onFieldChange('adjective_1', event.target.value)}}
-                value=''
-                />
-            </p>
+      <form  onSubmit={ this.onSubmit } className="new-student-form">
+      <div>
+      <p>
+      <label htmlFor="adjective_1">adjective_1:</label>
+      <input
+      adjective_1 = "adjective_1"
+      onChange={(event) => { this.onFieldChange('adjective_1', event.target.value)}}
+      value=''
+      />
+      </p>
 
-            <p>
-              <label htmlFor="adjective_2">adjective_2:</label>
-              <input
-                adjective_2 = "adjective_2"
-                onChange={(event) => { this.onFieldChange('adjective_2', event.target.value)}}
-                value=''
-                />
-            </p>
+      <p>
+      <label htmlFor="adjective_2">adjective_2:</label>
+      <input
+      adjective_2 = "adjective_2"
+      onChange={(event) => { this.onFieldChange('adjective_2', event.target.value)}}
+      value=''
+      />
+      </p>
 
-            <p>
-              <label htmlFor="noun_1">noun_1: </label>
-              <input
-                noun_1 = "noun_1"
-                onChange={(event) => { this.onFieldChange('noun_1', event.target.value)}}
-                value=''
-                />
-            </p>
+      <p>
+      <label htmlFor="noun_1">noun_1: </label>
+      <input
+      noun_1 = "noun_1"
+      onChange={(event) => { this.onFieldChange('noun_1', event.target.value)}}
+      value=''
+      />
+      </p>
 
-            <p>
-              <label htmlFor="noun_2">noun_2: </label>
-              <input
-                noun_2 = "noun_2"
-                onChange={(event) => { this.onFieldChange('noun_2', event.target.value)}}
-                value=''
-                />
-            </p>
+      <p>
+      <label htmlFor="noun_2">noun_2: </label>
+      <input
+      noun_2 = "noun_2"
+      onChange={(event) => { this.onFieldChange('noun_2', event.target.value)}}
+      value=''
+      />
+      </p>
 
-            <input
-              className="button success"
-              type="submit"
-              value="get MadLib"
-              />
-          </div>
-        </form>
+      <input
+      className="button success"
+      type="submit"
+      value="get MadLib"
+      />
       </div>
-        {this.props.words}
-        {this.props.updateWord}
+      </form>
+      </div>
+      {this.props.words}
+      {this.props.updateWord}
       </div>
     );
   }
