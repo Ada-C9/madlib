9b655b54b3a654121245c1b48486a10d8b5917e4
Phoebe <phoebe.manning8@gmail.com>
2018-06-01 14:28:30 -0700

    created Dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class Dropdown extends Component {
+  render(){
+    return(
+      <select>
+        
+      </select>
+    );
+  }
+}
+
+export default Dropdown;
