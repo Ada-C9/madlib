ffa1e6fdc107bd1e8cf1e2ba6d80ff77390d9de4
SelamawitA <Selamawit.ainalem@gmail.com>
2018-06-01 22:07:59 -0700

    added form.js and ran out of time before implementing currentMatlib function to form.js file. Plan will be to pass function as a prop to form.js. Further details of implementation are provided in notes.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import MadLibs from './madlibs/MadLibs.js';
 import Story from './components/Story.js';
-
+import Form from './components/Form.js';
 class App extends Component {
   constructor() {
     super();
@@ -11,11 +11,11 @@ class App extends Component {
       selectedMadLib: MadLibs[0]
     };
   }
-
   // Update the value of a word in the selected
   // mad lib using setState
   updateWord(key, value) {
     const updatedMadLib = this.state.selectedMadLib;
+
     const changedWord = updatedMadLib.words.find((word) => {
       return word.key === key
     });
@@ -23,18 +23,32 @@ class App extends Component {
     this.setState({selectedMadLib: updatedMadLib});
   }
 
+//form keeps track of individual word content
+//app keeps track of stories coming in.
+
+  currentMadlib = () => {
+    const aMadlib = this.state.selectedMadLib;
+    //loop through the string and update the content with values from the form?
+    //pass content from form into getWord , display.
+
+    //pass this function as a prop to the Form -
+    //search for itms within the getWord() function of the object.
+    //display that function
+  }
+
   render() {
     return (
       <section className="App">
         <h1>Welcome to MadLibs!</h1>
         <p>Fill in all of the choices to see your final story.</p>
-        {/*
-          Render your form with input values
-        */}
+        <Form />
+
         <Story
           title={ this.state.selectedMadLib.title }
           text={ this.state.selectedMadLib.getText() }
           />
+
+
       </section>
     );
   }
