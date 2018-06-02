0e3385abfd6622ee865c697b6f11488d6a5db54e
Dikla <dikla.aharoni@gmail.com>
2018-06-01 17:52:22 -0700

    Created the form

--- src/components/MadlibForm.js
+++ src/components/MadlibForm.js
@@ -7,15 +7,14 @@ class MadlibForm extends Component {
     addStoryCallback: PropTypes.func
   }
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {
-      adjective_1: '',
-      adjective_2: '',
-      noun_1: '',
-      noun_2: '',
-    }
+    let initialState = {};
+    props.input.words.forEach((word) => {
+      initialState[word.key] = "";
+    });
+    this.state = initialState;
   }
 
   onInputChange = (event) => {
@@ -28,71 +27,43 @@ class MadlibForm extends Component {
   onFormSubmit = (event) => {
     event.preventDefault();
     console.log("Form submition");
-    const adjective_1 = this.state.adjective_1;
-    const adjective_2 = this.state.adjective_2;
-    const noun_1 = this.state.noun_1;
-    const noun_2 = this.state.noun_2;
-    this.props.addStoryCallback(adjective_1, adjective_2, noun_1, noun_2);
 
-    this.setState({
-      adjective_1: '',
-      adjective_2: '',
-      noun_1: '',
-      noun_2: '',
+    this.props.input.words.forEach((word) => {
+      this.props.addStoryCallback(word.key, this.state[word.key])
+      this.setState({[word.key]: ""});
     });
+
+
+
   }
 
   render() {
+
+    const words = this.props.input.words;
+    const inputs = words.map((word, index) => {
+      return <div key={index}>
+        <input
+        name={word.key}
+        type="text"
+        placeholder = {word.key}
+        value={this.state[word.key]}
+        onChange={this.onInputChange}
+        />
+      </div>
+    })
+
+
+
     return (
       <div>
       <form
-      className="new-student-form"
       onSubmit={this.onFormSubmit}
       >
-        <div>
-          <label htmlFor="adjective_1"></label>
-          <input
-          name="adjective_1"
-          type="text"
-          placeholder = "adjective_1"
-          value={this.state.key}
-          onChange={this.onInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="adjective_2"></label>
-          <input
-          name="adjective_2"
-          type="text"
-          placeholder = "adjective_2"
-          value={this.state.lable}
-          onChange={this.onInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="noun_1"></label>
-          <input
-          name="noun_1"
-          type="text"
-          placeholder = "noun_1"
-          value={this.state.lable}
-          onChange={this.onInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="noun_2"></label>
-          <input
-          name="noun_2"
-          type="text"
-          placeholder = "noun_2"
-          value={this.state.lable}
-          onChange={this.onInputChange}
-          />
-        </div>
+      {inputs}
         <input
           className="button success"
           type="submit"
-          value="Add Student"
+          value="Update"
         />
       </form>
     </div>
