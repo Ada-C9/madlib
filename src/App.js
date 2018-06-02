440a2f62d12cc7860f164e63dd183d8d871b71ee
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-06-01 20:33:03 -0700

    added methods to pass in the new state from the form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import MadLibs from './madlibs/MadLibs.js';
 import Story from './components/Story.js';
+import GetWordsForm from "./components/GetWordsForm"
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      selectedMadLib: MadLibs[0]
+      selectedMadLib: MadLibs[Math.floor(Math.random() * MadLibs.length)]
     };
   }
 
@@ -23,18 +24,27 @@ class App extends Component {
     this.setState({selectedMadLib: updatedMadLib});
   }
 
+  onFormSubmit = (formState) => {
+    Object.keys(formState).forEach((key) => {
+      const value = formState[key];
+      this.updateWord(key, value);
+    })
+  }
+
+
   render() {
+    const wordsToFind = this.state.selectedMadLib.words;
+
+
     return (
       <section className="App">
-        <h1>Welcome to MadLibs!</h1>
-        <p>Fill in all of the choices to see your final story.</p>
-        {/*
-          Render your form with input values
-        */}
-        <Story
-          title={ this.state.selectedMadLib.title }
-          text={ this.state.selectedMadLib.getText() }
-          />
+      <h1>Welcome to MadLibs!</h1>
+      <p>Fill in all of the choices to see your final story.</p>
+      <GetWordsForm words={wordsToFind} updatewords={this.onFormSubmit}/>
+      <Story
+      title={ this.state.selectedMadLib.title }
+      text={ this.state.selectedMadLib.getText() }
+      />
       </section>
     );
   }
