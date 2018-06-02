7f7930fa9c112a568c9007d2ce6b6fea196e6c17
0velez0 <mariaivelez@gmail.com>
2018-06-01 18:20:15 -0700

    create form.js component

---
+++ src/components/Form.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+
+class Form extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Form;
